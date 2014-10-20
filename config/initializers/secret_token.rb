ed8bf04e54e67d72591520f35ba8510705a81258
Rafael Une <unesjc@gmail.com>
2014-10-20 21:11:19 -0200

    Remoção do secret da lista de ignorados

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Casamentosite::Application.config.secret_key_base = 'e91f906ff21f27bc2cdabfda43eff33f594c1601e9c78cb0f88eb081e9c05f2faf61266c3946bec9114b8871dcdd61a193ea11dfb394835b2ca3a9fab3e7d773'
